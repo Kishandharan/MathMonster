0da169e6f48b85aeeca7659715fb82b0290ca2cc
Kishan <152014111+Kishandharan@users.noreply.github.com>
2026-05-14 17:12:34 +0530

    Successfully connected the frontend and the API

--- frontend.js
+++ frontend.js
@@ -6,8 +6,8 @@ const end = document.getElementById("end");
 const score = document.getElementById("score");
 const history = document.getElementById("history");
 
-// For now, we are using a temporary dataset instead of the Flask API 
-const dataset = {
+// temporary dataset 
+/*const dataset = {
   cap: 4,
   questions: [
     {q: "What is 1+1", a:"2"},
@@ -15,24 +15,17 @@ const dataset = {
     {q: "What is 1+3", a:"4"},
     {q: "What is 1+4", a:"5"},
   ]
-};
+};*/
 
-let count = 0;
-let points = 0;
-let limit = dataset["cap"];
-question.textContent = dataset["questions"][0]["q"];
+fetch("http://localhost:5000").then((value) => value.json()).then((dataset) => {
+  console.log(dataset);
+  let count = 0;
+  let points = 0;
+  let limit = dataset["cap"];
+  question.textContent = dataset["questions"][0]["q"];
 
-function handle() {
-  if(count+1 < limit){
-    if(answer.value === dataset["questions"][count]["a"]){
-      points+=10;
-      history.innerHTML += `<p>Q${count+1} - 10</p>`;
-    }else{
-      points+=0;
-      history.innerHTML += `<p style="color: #FF0000;">Q${count+1} - 0</p>`;
-      alert("Oops! Incorrect answer!");
-    }
-  }else{
+  function handle() {
+    if(count+1 < limit){
       if(answer.value === dataset["questions"][count]["a"]){
         points+=10;
         history.innerHTML += `<p>Q${count+1} - 10</p>`;
@@ -41,18 +34,29 @@ function handle() {
         history.innerHTML += `<p style="color: #FF0000;">Q${count+1} - 0</p>`;
         alert("Oops! Incorrect answer!");
       }
-      main.style.display = "none";
-      end.style.opacity = "1";
-      score.textContent = points.toString();
+    }else{
+        if(answer.value === dataset["questions"][count]["a"]){
+          points+=10;
+          history.innerHTML += `<p>Q${count+1} - 10</p>`;
+        }else{
+          points+=0;
+          history.innerHTML += `<p style="color: #FF0000;">Q${count+1} - 0</p>`;
+          alert("Oops! Incorrect answer!");
+        }
+        main.style.display = "none";
+        end.style.opacity = "1";
+        score.textContent = points.toString();
+    }
+    count++;
+    question.textContent = dataset["questions"][count]["q"];
+    answer.value = "";
   }
-  count++;
-  question.textContent = dataset["questions"][count]["q"];
-  answer.value = "";
-}
 
-submit.onclick = handle;
-answer.addEventListener("keydown", function (event){
-  if(event.key == "Enter"){
-    handle();
-  }
+  submit.onclick = handle;
+  answer.addEventListener("keydown", function (event){
+    if(event.key == "Enter"){
+      handle();
+    }
+  });
 });
+
